e6d52837fd6c55d79ae3dbcf797b90815862e1ce
lucas <lucasmastro93@gmail.com>
2022-09-20 19:09:15 -0300

    modificando el DOM y agregando Login con LS y SS

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,12 +1,12 @@
 const autos = [
-    { id: 1, modelo: "207", marca: "peugeot", precio: "2500000", img: "207.jpg" },
-    { id: 2, modelo: "rcz", marca: "peugeot", precio: "3500000", img: "rcz.jpg" },
-    { id: 3, modelo: "x6", marca: "bmw", precio: "4500000", img: "x6.jpg" },
-    { id: 4, modelo: "corolla", marca: "toyota", precio: "2400000", img: "corolla.jpg" },
-    { id: 5, modelo: "focus", marca: "ford", precio: "2700000", img: "focus.jpg" },
-    { id: 6, modelo: "cruze", marca: "chevrolet", precio: "2900000", img: "cruze.jpg" },
-    { id: 7, modelo: "mito", marca: "alfa romeo", precio: "3700000", img: "mito.jpg" },
-    { id: 8, modelo: "clio", marca: "renault", precio: "1600000", img: "clio.jpg" },
+    { id: 1, modelo: "207", marca: "peugeot", precio: "2500000", img: "./img/207.jpg" },
+    { id: 2, modelo: "rcz", marca: "peugeot", precio: "3500000", img: "./img/rcz.jpg" },
+    { id: 3, modelo: "x6", marca: "bmw", precio: "4500000", img: "./img/x6.jpg" },
+    { id: 4, modelo: "corolla", marca: "toyota", precio: "2400000", img: "./img/corolla.jpg" },
+    { id: 5, modelo: "focus", marca: "ford", precio: "2700000", img: "./img/focus.jpg" },
+    { id: 6, modelo: "cruze", marca: "chevrolet", precio: "2900000", img: "./img/cruze.jpg" },
+    { id: 7, modelo: "mito", marca: "alfa romeo", precio: "3700000", img: "./img/mito.jpg" },
+    { id: 8, modelo: "clio", marca: "renault", precio: "1600000", img: "./img/clio.jpg" },
 ];
 
 console.log(autos);
@@ -29,9 +29,37 @@ const a2 = document.getElementById("a2");
 const a3 = document.getElementById("a3");
 const botonSearch = document.querySelector("#botonSearch");
 const inputSearch = document.querySelector("#inputSearch");
-const contenedor = document.querySelector('.contenedor')
+const contenedor = document.querySelector('.contenedor');
+const contenidoCards = document.querySelector('.contenido__cards');
+const inputEmail = document.querySelector('.inputEmail');
+const inputPassword = document.querySelector('.inputPassword');
+const botonSearchLogin = document.getElementById("botonSearchLogin");
+const checkboxLogin = document.querySelector('.checkboxLogin');
+
 
 h1.innerText = "Probando contenido dinamico";
+// agregar las CARDS MEDIANTE JS
+    
+function crearCards() {
+    autos.forEach(auto => {
+
+        const cardCreate = document.createElement('div')
+        const contenidoCard = ` <div class="col">
+        <div class="card p-3" style="width: 18rem;">
+          <img src=${auto.img} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${auto.marca}</h5>
+            <p class="card-text">${auto.modelo}</p>
+            <h5 class="card-text2">Precio: ${auto.precio}$</h5>
+            <a href="#" class="btn btn-primary agregarAfavoritos" id="a">Agregar a favoritos</a>
+          </div>
+        </div>
+      </div>`;
+        cardCreate.innerHTML = contenidoCard
+        contenidoCards.append(cardCreate);
+    });
+}
+crearCards();
 
 // AGREGANDO CONTENIDO A FAVORITOS MEDIANTE EVENTO CLICK
 
@@ -56,8 +84,7 @@ function agregarAfavoritosClicked(event) {
 function agregandoAfavoritos(card, cardImg, cardTitle, cardText, cardText2) {
     const cardDiv = document.createElement('div')
     const cardContenido = `
-    <div class="row favoritos__contenido contenedor">
-          
+    <div class="favoritos__contenido contenedor"> 
           <div class="card p-3 " style="width: 18rem;">
             <img src=${cardImg} class="card-img-top" alt="...">
             <div class="card-body">
@@ -65,15 +92,26 @@ function agregandoAfavoritos(card, cardImg, cardTitle, cardText, cardText2) {
               <p class="card-text">${cardText}</p>
               <h5 class="card-text2">${cardText2}</h5>
               
-              <button type="button" class="btn btn-danger">X</button>
+              <button type="button" class="btn btn-danger botonDelete">X</button>
             </div>
 
           </div>
     `;
     cardDiv.innerHTML = cardContenido
     contenedor.append(cardDiv);
-}
 
+    cardDiv.querySelector('.botonDelete').addEventListener('click', eliminarCard)
+    
+  
+
+    
+}
+// funcion de ELIMINAR CARTA 
+
+function eliminarCard(event) {
+    const buttonClick = event.target
+    buttonClick.closest('.card').remove();
+}
 
 // BUSQUEDA Y FILTRADO POR CONSOLA MEDIANTE EVENT CLICK
 
@@ -97,82 +135,37 @@ inputSearch.addEventListener('input', () => {
 })
 
 
+// LOGIN
 
-
-/*
-const nuevoAuto = new auto(9, "focus", "ford", "3000000");
-
-function cargarAuto(arr,valor) {
-    arr.push(valor);
+function guardadoEnLSoST(valor) {
+    let user = {username: inputEmail.value, password: inputPassword.value }
+if (valor === "sessionStorage") {
+    sessionStorage.setItem("user", JSON.stringify(user));
 }
-cargarAuto(autos,nuevoAuto)
-
-console.log(autos);
-
-for (const objeto of autos) {
-    console.log(objeto);
+if (valor === "localStorage"){
+    localStorage.setItem("user", JSON.stringify(user));
+}
+return user
 }
 
-let preguntaAgregarAuto = prompt("Quieres agregar un nuevo auto?"
-+ " Selecciona un opción \n 1 - No \n 2 - Si");
-
-if (preguntaAgregarAuto > 1) {
-   
-        let entrada = prompt("ingresa modelo");
-        autos.push(entrada);
-        let entrada2 = prompt("ingresa marca");
-        autos.push(entrada2);
-        let entrada3 = prompt("ingresa precio");
-        autos.push(entrada3);
-        console.log(autos);
-
-} else{
-    for (const objeto of autos) {
-        console.log(objeto);
-    }
+function recover(datos){
+if(datos){
+    inputEmail.value = datos.username;
+    inputPassword.value = datos.password;
+}
 }
 
+recover(JSON.parse(localStorage.getItem("user")));
 
-const resultado = autos.some((el) => el.modelo == "onix")
-const resultado2 = autos.some((el) => el.modelo == "celta")
-
-
-console.log(resultado);
-console.log(resultado2);
-
-
-const precio = autos.map((precios)=>{
-
-    return precios.marca;
+botonSearchLogin.addEventListener('click',(e)=>{
+e.preventDefault();
+if (checkboxLogin.checked){
+    guardadoEnLSoST("localStorage");
+    console.log("guardado en Local");
+} else {
+    guardadoEnLSoST("sessionStorage");
+    console.log("guardado en Sesion");
+}
 
 });
-
-const actualizarPrecio = autos.map ((precios)=>{
-return {
-    id: precios.id,
-    modelo: precios.modelo,
-    marca: precios.marca,
-    precio: precios.precio * 2 ,
-
-};
-
-
-});
-
-console.log(actualizarPrecio);
-
-
-
-const masBarato = autos.filter (modelo => modelo.precio < 3000000)
-
-console.log(masBarato);
-*/
-
-
-
-
-
-
-
-
 
